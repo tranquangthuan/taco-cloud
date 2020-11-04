378d105d27c97b36da9b231992f115a6c8c048f1
Thuan <thuan.uit@gmail.com>
2020-11-04 22:53:50 +0700

    Fisrt commit

---
+++ src/main/java/com/thuan/spring/core/TacoCloudApplication.java
@@ -0,0 +1,13 @@
+package com.thuan.spring.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TacoCloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TacoCloudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thuan/spring/core/TacoCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thuan.spring.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TacoCloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
